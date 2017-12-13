a135e8731a7234bc78609c50c8ba783eef8727bb
Mark Munsayac <munsayacmlj@gmail.com>
2017-12-14 03:35:50 +0800

    final capstone 1

--- capstone1/js/script.js
+++ capstone1/js/script.js
@@ -4,5 +4,24 @@ $(document).ready(function (){
       $('.fadein :first-child').fadeOut()
          .next('img').fadeIn()
          .end().appendTo('.fadein');}, 
-      4000);
+      4000);    
 });
+
+
+ window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if ((document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) && (screen.width > 1023))  {
+            document.getElementById("myBtn").style.display = "block";
+        } else {
+            document.getElementById("myBtn").style.display = "none";
+        }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+//        document.body.scrollTop = 0;
+//        document.documentElement.scrollTop = 0;
+        $("html, body").animate({ scrollTop: 0 }, 'slow');
+        return false;
+    }
