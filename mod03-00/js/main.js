e7a75c1f23a3a6f13c9523e429cae08e4ad5430b
laindesiel <munsayacmlj@gmail.com>
2018-01-05 16:56:47 +0800

    square game

---
+++ mod03-00/js/main.js
@@ -0,0 +1,14 @@
+
+
+
+function alrt(){
+    alert("HERRO");
+}
+
+function changeHTML(){
+    document.getElementById('head2').innerHTML = "HELLO WORLD";
+}
+
+//document.getElementById('btn').onclick = alrt(); 
+    
+    
